9b123fb892b105b6fa185f70471ebc744141935d
Djakovic <DDjakovic@CCLLABEL.CCLIND.NET>
2018-05-08 11:25:01 -0400

    problem 1 solved

--- 1.py
+++ 1.py
@@ -2,5 +2,14 @@
 
 #Problem 1
 
-def addFrom(number):
-	
+numbersToAdd = []
+
+def addTo(number):
+	count = 0
+	while count < number:
+		if count % 3 == 0 or count % 5 == 0:
+			numbersToAdd.append(count)
+		count = count + 1
+	#print(numbersToAdd)
+	print(sum(numbersToAdd))
+addTo(1000)
